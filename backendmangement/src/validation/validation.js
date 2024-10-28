63b134731668b2f4d33ef56e43b81106036df3cf
GRINTEX <171ankit.2020@gmail.com>
2024-10-28 14:48:49 +0530

    add

--- backendmangement/src/validation/validation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-const taskValidaton = Joi.object().keys({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    deadline: Joi.string().required()
-});
-
-module.exports = { taskValidaton };
